948b1cc596e176090ce22439e61313f8f8bd1548
Ruslan <darknesselll@gmail.com>
2021-10-12 13:45:47 +0600

    second KEK

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -4,6 +4,28 @@ assert capitalize('hello') == 'Hello'
 
 assert capitalize('') == ''
 
+def fill(coll, value, begin=0, end=None):
+    chunk = [value for _ in coll[begin:end]]
+    coll[begin:end] = chunk
+
+def test_fill_default(collection, fill):
+    fill(collection, '*')
+    assert collection == ['*', '*', '*', '*']
+
+
+def test_fill_start_ge_length(collection, fill):
+    fill(collection, '*', 10, 12)
+    assert collection == [1, 2, 3, 4]
+
+
+def test_fill_start_ge_end(collection, fill):
+    fill(collection, '*', 2, 2)
+    assert collection == [1, 2, 3, 4]
+
+
+def test_fill_end_ge_length(collection, fill):
+    fill(collection, '*', 0, 10)
+    assert collection == ['*', '*', '*', '*']
 
 # if get({'hello': 'world'}, 'hello') != 'world':
 #     raise Exception('wrong!!!')
